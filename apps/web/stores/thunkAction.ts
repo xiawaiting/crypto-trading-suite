24abf3ca79a49ae04ca92d97934f3b495b5d1daf
xiawaiting <xiawaiting@proton.me>
2022-06-25 12:33:03 +0000

    Update cc.md

--- apps/web/stores/thunkAction.ts
+++ apps/web/stores/thunkAction.ts
@@ -17,4 +17,67 @@ export const fetchStgOrders = createAsyncThunk(
       stgOrders: { currentPage, pageSize },
     } = state.appStore
     const res = await strategyApi.getStgOrders({
-      currentPage: stgOrdersPar
+      currentPage: stgOrdersParams.current || currentPage,
+      pageSize: stgOrdersParams.page || pageSize,
+      symbol: asset,
+      is_running: status,
+    })
+    if (res.code === SUCCESS) {
+      return res
+    } else {
+      // message.error(res.msg || 'error')
+      // throw new Error(res.msg || 'error')
+      return res
+    }
+  },
+)
+
+export const fetchSpotOrders = createAsyncThunk(
+  'fetchSpotOrders',
+  async (spotOrdersParams: SpotOrdersParams, thunkAPI) => {
+    const state: RootState = thunkAPI.getState() as RootState
+    const {
+      spotFilter: { symbol },
+      spotOrders: { currentPage, pageSize },
+    } = state.appStore
+
+    const res = await spotApi.getSpotOrders({
+      currentPage: spotOrdersParams.current || currentPage,
+      pageSize: spotOrdersParams.page || pageSize,
+      symbol,
+    })
+    if (res.code === SUCCESS) {
+      return res
+    } else {
+      console.log('fetchSpotOrders error:', res)
+      // message.error(res.msg || 'error')
+      // throw new Error(res.msg || 'error')
+      return res
+    }
+  },
+)
+
+export const fetchFutureOrders = createAsyncThunk(
+  'fetchFutureOrders',
+  async (options: FutureOrdersParams, thunkAPI) => {
+    const state: RootState = thunkAPI.getState() as RootState
+    const {
+      futureFilter: { symbol },
+      futureOrders: { currentPage, pageSize },
+    } = state.appStore
+
+    const res = await futureApi.getFutureOrders({
+      currentPage: options.current || currentPage,
+      pageSize: options.page || pageSize,
+      symbol,
+    })
+    if (res.code === SUCCESS) {
+      return res
+    } else {
+      console.log('fetchSpotOrders error:', res)
+      // message.error(res.msg || 'error')
+      // throw new Error(res.msg || 'error')
+      return res
+    }
+  },
+)
